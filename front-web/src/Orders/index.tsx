2f529ad833d208c8fc08ac249cafe2fc72a6df43
Gabriel Mendes <dev_gabrielsouza@hotmail.com>
2021-01-07 00:40:57 -0300

    Adicionando rotas e cabeçalhos

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StepsHeader from './StepsHeader';
+import './style.css';
+
+function Orders(){
+    return (
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders 
