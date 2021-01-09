c826cd0954e78f94ab61a68c126f176cf387afd3
Gabriel Mendes <dev_gabrielsouza@hotmail.com>
2021-01-09 18:34:11 -0300

    término do front-web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from "../api";
-import { OrderLocationdata } from "./types";
+import { OrderLocationData } from "./types";
 
 const initialPosition = {
     lat: -13.001808,
@@ -19,7 +19,7 @@ type Place = {
 }
 
 type Props = {
-  onChangeLocation: (location: OrderLocationdata) => void;
+  onChangeLocation: (location: OrderLocationData) => void;
 }
 
 function OrderLocation({ onChangeLocation }: Props) {
@@ -57,7 +57,7 @@ function OrderLocation({ onChangeLocation }: Props) {
     <div className="order-location-container">
       <div className="order-location-content">
         <h3 className="order-location-title">
-          Selecione onde o pedido deve seer entregue:
+          Selecione onde o pedido deve ser entregue:
         </h3>
         <div className="filter-container">
             <AsyncSelect  
